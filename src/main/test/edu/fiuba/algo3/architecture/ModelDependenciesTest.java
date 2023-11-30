365c2f17bd768a35336565382c49a1a15d260bf8
fnpratto <fnpratto@gmail.com>
2023-11-30 16:50:43 -0300

    cambio en modeldependencies

--- src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
+++ src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
@@ -14,11 +14,13 @@ public class ModelDependenciesTest {
 
     private final String JSON = "org.junit..";
 
+    private final String LOGGER = "org.apache..";
+
     @Test
     public void elModeloSoloPuedeReferenciarClasesDelModeloAdemasJavaAdemasJunit() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("edu.fiuba.algo3.modelo");
 
-        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT, JSON};
+        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT, JSON, LOGGER};
 
         ArchRule myRule = classes().that().resideInAPackage(MODEL)
                 .should().onlyDependOnClassesThat().resideInAnyPackage(listOfPackages);
